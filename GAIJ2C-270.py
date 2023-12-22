6af169553b84a1a412b943979141a40daaf2e071
Harbir Singh <130560577+mck-harbir@users.noreply.github.com>
2023-12-22 13:48:26 +0530

    AI model created GAIJ2C-270.py

---
+++ GAIJ2C-270.py
@@ -0,0 +1,43 @@
+using Grand.Business.Core.Interfaces.Catalog.Categories;
+using Grand.Business.Core.Interfaces.Catalog.Products;
+using Grand.Business.Core.Interfaces.Common.Directory;
+using Grand.Business.Core.Interfaces.Common.Localization;
+using Grand.Business.Core.Interfaces.Common.Logging;
+using Grand.Business.Core.Interfaces.Common.Seo;
+using Grand.Business.Core.Interfaces.Customers;
+using Grand.Business.Core.Interfaces.Directory;
+using Grand.Business.Core.Interfaces.Localization;
+using Grand.Business.Core.Interfaces.Logging;
+using Grand.Business.Core.Interfaces.Seo;
+using Grand.Business.Core.Utilities.Common.Security;
+using Grand.Domain.Catalog;
+using Grand.Domain.Common;
+using Grand.Domain.Customers;
+using Grand.Domain.Seo;
+using Grand.Infrastructure;
+using Grand.Web.Admin.Extensions;
+using Grand.Web.Admin.Interfaces;
+using Grand.Web.Admin.Models.Catalog;
+using Grand.Web.Common.DataSource;
+using Grand.Web.Common.Filters;
+using Grand.Web.Common.Security.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+
+```csharp
+/// <summary>
+/// Returns a view with the social media buttons for the specified product.
+/// </summary>
+/// <param name="model">The product model.</param>
+/// <returns>The view with the social media buttons.</returns>
+public virtual IActionResult ProductSocialMediaButtons(ProductModel model)
+{
+    // Get the product by its ID
+    var product = _productService.GetProductById(model.Id);
+
+    // Check if the product exists
+    if (product == null)
+    {
+        return Redirect
